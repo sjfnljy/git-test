8de1a7cbf38c990e009aa4cb924dd19ac06a84f7
sjf <2439477146@qq.com>
2023-02-02 15:17:48 +0800

    hot-fix test

--- src/main/java/com/sjf/git/demo01.java
+++ src/main/java/com/sjf/git/demo01.java
@@ -4,5 +4,6 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("hello,hot-fix");
+        System.out.println("hot-fix test");
     }
 }
